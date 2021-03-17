37a4fc19869ad6e28b05200b8d068ff43b8a5050
Bradley Berger <bradley.s.berger@gmail.com>
2021-03-17 09:11:12 -0700

    small cleanup changes - Add comments and reorganize the code

--- exercise_02/price_calculator.rb
+++ exercise_02/price_calculator.rb
@@ -1,17 +1,18 @@
 #!/bin/env/ruby
-require "./price_list.rb"
 # execute as: `ruby price_calculator.rb`
-# prompt user for food list
+require "./price_list.rb"
 puts "Hello, what did you purchase today?"
 puts "please list out all your items in a comma-delimited list"
-# clean up user input into an array of strings, eliminating empty elements
+# # #
+# Prompt user for input and parse into an array of strings, eliminating empty elements
 list = gets.split(',').map(&:strip).reject(&:empty?)
 
 if list.length == 0
   abort("It looks like you didn't buy anthing today")
 end
 
-# sort the input
+# # #
+# sort the input to get quantities
 sorted_hash = list.inject({}) do |hash, item|
   key = item.downcase.to_sym
   hash[key] = hash.keys.include?(key) ? hash[key] + 1 : 1
@@ -30,7 +31,8 @@ final_result = sorted_hash.inject({}) do |result, (item, quantity)|
   next result unless price_hash # skip unrecognized items
 
   if price_hash.keys.include? :sale
-    # this item has some kind of bogo pricing.
+    # this item has a buy `n` for the price of `x` pricing.
+    # figure out how many items should get sale price:
     sale_items = quantity / price_hash[:sale][:quantity]
     total_price += sale_items * price_hash[:sale][:price]
     # remainder of items are full price:
@@ -44,16 +46,21 @@ end
 
 puts "final_result: #{final_result}" if ENV['DEBUG']
 
-col_widths = [8,10,8]
-# simple function to add padding to string for output formatting.
-right_pad = ->(string, col_width) {
-  string + ' ' * (col_width - string.length)
-}
 item_rows = final_result.map do |item, result|
   [item.to_s, result[:quantity].to_s, "$#{result[:total_price].round(2)}"]
 end
+total_price = final_result.values.reduce(0) { |sum, hash| sum += hash[:total_price] }
+savings = (non_sale_price - total_price).round(2)
+
 # #
-# Format the output:
+# simple lambda to add padding to string for output formatting.
+right_pad = ->(string, col_width) {
+  string + ' ' * (col_width - string.length)
+}
+col_widths = [8,10,8]
+# # #
+# Format and print out the output:
+# #
 puts ''
 puts "Item    Quantity  Price   "
 puts "--------------------------"
@@ -62,7 +69,6 @@ item_rows.each do |row|
   puts right_pad.call(item, col_widths[0]) + right_pad.call(quantity, col_widths[1]) + right_pad.call(price, col_widths[2])
 end
 puts ''
-total_price = final_result.values.reduce(0) { |sum, hash| sum += hash[:total_price] }
-savings = (non_sale_price - total_price).round(2)
 puts "Total price : $#{total_price}"
 puts "You saved $#{savings} today."
+puts ''
